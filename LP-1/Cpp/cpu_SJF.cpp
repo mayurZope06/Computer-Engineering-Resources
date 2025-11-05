3b7644df5c2bf14dd37b0670b8f3e195aa4d4666
Mayur Zope <98807448+mayurZope06@users.noreply.github.com>
2025-11-05 12:44:19 +0530

    Update cpu_SJF.cpp

--- LP-1/Cpp/cpu_SJF.cpp
+++ LP-1/Cpp/cpu_SJF.cpp
@@ -36,3 +36,94 @@ int main() {
     cout << "\nAverage Turnaround Time -- " << tatavg / n;
     return 0;
 }
+
+// OR
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    int pid[n], at[n], bt[n], rt[n]; 
+    int wt[n], tat[n], cmplt = 0, t = 0, mn = 1e9, shortest = 0, finish_time;
+    bool check = false;
+
+    cout << "\nEnter Process ID, Arrival Time & Burst Time:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> pid[i] >> at[i] >> bt[i];
+        rt[i] = bt[i];
+    }
+
+    cout << "\nOrder of execution:\n";
+
+    while (cmplt != n) {
+        mn = 1e9;
+        check = false;
+
+        for (int i = 0; i < n; i++) {
+            if ((at[i] <= t) && (rt[i] < mn) && rt[i] > 0) {
+                mn = rt[i];
+                shortest = i;
+                check = true;
+            }
+        }
+
+        if (!check) {
+            t++;
+            continue;
+        }
+
+        // Execute the selected process
+        rt[shortest]--;
+        cout << "P" << pid[shortest] << " ";
+
+        if (rt[shortest] == 0) {
+            cmplt++;
+            finish_time = t + 1;
+
+            wt[shortest] = finish_time - bt[shortest] - at[shortest];
+            if (wt[shortest] < 0) 
+                wt[shortest] = 0;
+
+            tat[shortest] = finish_time - at[shortest];
+        }
+        t++;
+    }
+
+    cout << "\n\nPID\tAT\tBT\tWT\tTAT\n";
+    float avgWT = 0, avgTAT = 0;
+
+    for (int i = 0; i < n; i++) {
+        cout << pid[i] << "\t" << at[i] << "\t" << bt[i] << "\t" << wt[i] << "\t" << tat[i] << endl;
+        avgWT += wt[i];
+        avgTAT += tat[i];
+    }
+
+    cout << "\nAverage Waiting Time = " << avgWT / n;
+    cout << "\nAverage Turnaround Time = " << avgTAT / n << endl;
+
+    return 0;
+}
+
+// Enter number of processes: 4
+
+// Enter Process ID, Arrival Time & Burst Time:
+// 1 0 5
+// 2 1 3
+// 3 2 4
+// 4 4 1
+
+// Order of execution:
+// P1 P2 P2 P2 P4 P1 P1 P1 P1 P3 P3 P3 P3 
+
+// PID	AT	BT	WT	TAT
+// 1	0	5	4	9
+// 2	1	3	0	3
+// 3	2	4	7	11
+// 4	4	1	0	1
+
+// Average Waiting Time = 2.75
+// Average Turnaround Time = 6
