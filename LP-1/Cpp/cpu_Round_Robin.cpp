bb82961f1ee905ad77b26d0f6db8e8041a91c44a
Mayur Zope <98807448+mayurZope06@users.noreply.github.com>
2025-11-05 13:14:59 +0530

    Update cpu_Round_Robin.cpp

--- LP-1/Cpp/cpu_Round_Robin.cpp
+++ LP-1/Cpp/cpu_Round_Robin.cpp
@@ -40,3 +40,112 @@ int main() {
         cout << "\t" << i + 1 << " \t " << ct[i] << " \t\t " << wa[i] << " \t\t " << tat[i] << "\n";
     return 0;
 }
+
+// OR
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n, tq;
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    int p[n], at[n], bt[n], rt[n], wt[n], tat[n], ct[n];
+
+    for(int i = 0; i < n; i++) {
+        cout << "Enter Arrival Time & Burst Time for P" << i+1 << ": ";
+        cin >> at[i] >> bt[i];
+        p[i] = i+1;
+        rt[i] = bt[i];
+        ct[i] = -1;
+    }
+
+    cout << "Enter Time Quantum: ";
+    cin >> tq;
+
+    queue<int> q;
+    int time = 0, completed = 0;
+
+    // Push first arriving process
+    int idx = 0;
+    while(idx < n && at[idx] <= time) {
+        q.push(idx);
+        idx++;
+    }
+
+    cout << "\nGantt Chart:\n";
+
+    while(completed < n) {
+        if(q.empty()) {
+            time++;
+            cout << "| IDLE ";
+            while(idx < n && at[idx] <= time) {
+                q.push(idx);
+                idx++;
+            }
+            continue;
+        }
+
+        int i = q.front(); 
+        q.pop();
+
+        cout << "| P" << p[i] << " ";
+
+        if(rt[i] > tq) {
+            time += tq;
+            rt[i] -= tq;
+        } else {
+            time += rt[i];
+            rt[i] = 0;
+            ct[i] = time;
+            completed++;
+        }
+
+        while(idx < n && at[idx] <= time) {
+            q.push(idx);
+            idx++;
+        }
+
+        if(rt[i] > 0) q.push(i);
+    }
+    cout << "|\n";
+
+    float avgWT = 0, avgTAT = 0;
+
+    cout << "\nProcess\tAT\tBT\tWT\tTAT";
+    for(int i = 0; i < n; i++) {
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+        avgWT += wt[i];
+        avgTAT += tat[i];
+
+        cout << "\nP" << p[i] << "\t" << at[i] << "\t" << bt[i]
+             << "\t" << wt[i] << "\t" << tat[i];
+    }
+
+    cout << "\n\nAverage Waiting Time = " << avgWT/n;
+    cout << "\nAverage Turnaround Time = " << avgTAT/n;
+
+    return 0;
+}
+
+// Enter number of processes: 4
+// Enter Arrival Time & Burst Time for P1: 0 5
+// Enter Arrival Time & Burst Time for P2: 1 4
+// Enter Arrival Time & Burst Time for P3: 2 2
+// Enter Arrival Time & Burst Time for P4: 4 1
+// Enter Time Quantum: 2
+
+// Gantt Chart:
+// | P1 | P2 | P3 | P1 | P4 | P2 | P1 |
+
+// Process	AT	BT	WT	TAT
+// P1	0	5	7	12
+// P2	1	4	6	10
+// P3	2	2	2	4
+// P4	4	1	4	5
+
+// Average Waiting Time = 4.75
+// Average Turnaround Time = 7.75
