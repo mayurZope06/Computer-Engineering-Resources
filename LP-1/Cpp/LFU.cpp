0a9e7350e265534a2ce029ee578018395e058dfb
Mayur Zope <98807448+mayurZope06@users.noreply.github.com>
2025-11-05 12:27:24 +0530

    Optimal

--- LP-1/Cpp/LFU.cpp
+++ LP-1/Cpp/LFU.cpp
@@ -43,3 +43,102 @@ int main() {
     cout << "\n\n Total number of page faults -- " << pf << endl;
     return 0;
 }
+
+// OR
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rs[50], frames[10];
+    int n, f, i, j, k, pos, pageFault = 0;
+
+    cout << "Enter number of page references: ";
+    cin >> n;
+
+    cout << "Enter the reference string: ";
+    for(i = 0; i < n; i++)
+        cin >> rs[i];
+
+    cout << "Enter number of frames: ";
+    cin >> f;
+
+    for(i = 0; i < f; i++)
+        frames[i] = -1;
+
+    cout << "\nPage Replacement Process (OPTIMAL):\n";
+
+    for(i = 0; i < n; i++) {
+        bool found = false;
+
+        for(j = 0; j < f; j++) {
+            if(frames[j] == rs[i]) {
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            int farthest = -1;
+            pos = -1;
+
+            for(j = 0; j < f; j++) {
+                int nextUse = -1;
+
+                for(k = i + 1; k < n; k++) {
+                    if(frames[j] == rs[k]) {
+                        nextUse = k;
+                        break;
+                    }
+                }
+
+                if(nextUse == -1) {
+                    pos = j;
+                    break;
+                }
+
+                if(nextUse > farthest) {
+                    farthest = nextUse;
+                    pos = j;
+                }
+            }
+
+            frames[pos] = rs[i];
+            pageFault++;
+        }
+
+        for(j = 0; j < f; j++)
+            cout << frames[j] << "\t";
+
+        if(!found)
+            cout << "PF";
+
+        cout << endl;
+    }
+
+    cout << "\nTotal Page Faults using OPTIMAL = " << pageFault << endl;
+    return 0;
+}
+
+// Enter number of page references: 20
+// Enter the reference string: 7   
+// 0
+// 1
+// 2
+// 0
+// 3
+// 0
+// 4
+// 2
+// 3
+// 0
+// 3
+// 2
+// 1
+// 2
+// 0
+// 1
+// 7
+// 0
+// 1
+// Enter number of frames: 4
